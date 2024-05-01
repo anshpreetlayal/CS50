79efd695f5d7b5b61d0bdcb6881f56624499054b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-05-01 19:39:15 -0400

    updated code

--- arrays/scores.c
+++ arrays/scores.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int scores[3];
-    score[0] = 72;
-    score[1] = 73;
-    score[2] = 33;
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("score: ");
+    }
     printf("Average: %f\n", (score[0] + score[1] + score[2]) / 3.0);
 }
