b6dc355e1020f63281a6e66834213377859fdc55
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-05-01 19:45:06 -0400

    changes

--- c/arrays/scores.c
+++ c/arrays/scores.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
 
+const int N = 3;
 float average(int array[]);
 int main(void)
 {
-    int scores[3];
-    for (int i = 0; i < 3; i++)
+    int scores[N];
+    for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("score: ");
     }
