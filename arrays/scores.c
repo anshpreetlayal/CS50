a8cbd2769bf12f4e17fed8b3daa74a52edb249a2
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-05-01 19:32:49 -0400

    updated score code

--- arrays/scores.c
+++ arrays/scores.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
-
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    int scores[3];
+    score[0] = 72;
+    score[1] = 73;
+    score[2] = 33;
+    printf("Average: %f\n", (score[0] + score[1] + score[2]) / 3.0);
 }
