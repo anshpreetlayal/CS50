062cdbd9b45daa8ce9c22c1a71e086d26079e9dc
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-05-03 18:15:31 -0400

    added file

--- c/arrays/length.c
+++ c/arrays/length.c
@@ -1,14 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("what's your name? ");
-
+     int n = strlen(name)
     int n = 0;
-    while (name[n] != '\0')
-    {
-        n++;
-    }
     printf("%i\n", n);
 }
